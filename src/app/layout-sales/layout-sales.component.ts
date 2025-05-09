636e38ef2392632447241f04d4e56b74ce5389bc
Rafael Dias <rafaeldias@Mac.home>
2025-05-09 09:15:13 -0300

    feat:correcao de layout

--- src/app/layout-sales/layout-sales.component.ts
+++ src/app/layout-sales/layout-sales.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout-sales',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterModule],
   templateUrl: './layout-sales.component.html',
   styleUrls: ['./layout-sales.component.scss'],
 })
-export class LayoutSalesComponent {}
+export class LayoutSalesComponent {
+  constructor(private router: Router) {}
+
+  navigateToNewSale(): void {
+    this.router.navigate(['sales', 'new']);
+  }
+}
